db790c5dc1884f754a43afdafbf1ba0dab61269d
Nathan Owens <nathan.ab.owens@gmail.com>
2026-03-17 11:15:45 -0400

    Update Main method in BankRobbin.cs
    Refactor Main method to read input for locks and keys.

--- BankRobbin.cs
+++ BankRobbin.cs
@@ -1,4 +1,22 @@
-public static void Main(String[] args)
+using System;
+using System.Collections.Generic;
+
+public class program
 {
-  
+  public static void Main(String[] args)
+  {
+    int[] numLocks = new int[(int) System.ReadLine()];
+    int[] numKeys = new int[(int) System.ReadLine()];
+    String Lock = System.ReadLine();
+    String[][] possible = new String[numLocks][numKeys];
+    for(int i = 0; i < numLocks; i++)
+    {
+      for(int j = 0; j < numKeys; j++)
+      {
+      possible[i][j] = System.ReadLine();
+      }
+    }
+    
+  }
+
 }
