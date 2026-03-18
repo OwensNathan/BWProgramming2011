5ef3440c88a6d3fd03dc3e1b5fb69a94afc9ddf8
Nathan Owens <nathan.ab.owens@gmail.com>
2026-03-18 12:05:59 -0400

    Refactor TowerOfDefense to use public variables
    Refactored TowerOfDefense.cs to use public variables for rowCol and board, and modified the moves and towers initialization logic.

--- TowerOfDefense.cs
+++ TowerOfDefense.cs
@@ -7,21 +7,39 @@ public class HelloWorld
     {
         int rounds = int.Parse(Console.ReadLine());
         
-        string[] rowCol = Console.ReadLine()!.Split(' ');
+        public string[] rowCol = Console.ReadLine()!.Split(' ');
 
-        int[,] board = new int[int.Parse(rowCol[0]), int.Parse(rowCol[1])];
+        public int[,] board = new int[int.Parse(rowCol[0]), int.Parse(rowCol[1])];
         
         int moveNumber = int.Parse(Console.ReadLine());
         
         int[,] moves = new int[moveNumber,moveNumber];
         for(int i = 0; i < moveNumber; i++)
         {
-            for(int j = 0; j < moveNumber; j++)
-            {
-                string[] rowCol = Console.ReadLine()!.Split(' ');
-                
-                moves[i,j] = [int.Parse(rowCol[0]), int.Parse(rowCol[1])];
-            }
+            string[] rowCol2 = Console.ReadLine()!.Split(' ');
+            moves[int.Parse(rowCol2[0]), int.Parse(rowCol2[1])] = 1;
         }
+        
+        int towerNumber = int.Parse(Console.ReadLine());
+        int[,] towers = new int[towerNumber, towerNumber];
+        for(int i = 0; i < towerNumber; i++)
+        {
+            string[] rowCol2 = Console.ReadLine()!.Split(' ');
+            towers[int.Parse(rowCol2[0]), int.Parse(rowCol2[1])] = 2;
+        }
+        
+        int health = int.Parse(Console.ReadLine());
+        
+        int[,] damageSpots = DAMAGE(towers, moves);
+    }
+    
+    public int[,] DAMAGE(location, path)
+    {
+        int[,] damageLocations = [rowCol[0]+1, rowCol[1]+1];
+        for(int i = 0; i < rowCol[0]=1; i++)
+        {
+            
+        }
+        return damageLocations;
     }
 }
